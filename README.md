dbb4c555171a16af24cc00a40a00d592ab68c661
zhu yang <1794148812@qq.com>
2022-08-19 18:14:22 +0800

    8.19 model

--- README.md
+++ README.md
@@ -4,6 +4,8 @@
     <summary>目录</summary>
 
 - [ggplot](#ggplot)
+- [数据转换](#数据转换)
+- [数据探索分析](#数据探索分析)
 
 </details>
 
@@ -22,7 +24,7 @@ ggplot(data = <data>) +
 ```
 
 > `position`: 位置参数，三种`fill`, `identity`, `dodge`对应堆叠、精确重叠对象分组
-> `facet`: 分面，facet_wrap | facet_grid
+> `facet`: 分面，facet_wrap | facet_grid,需要用到函数指明分面变量
 > `coord`:坐标系，常见的 coord_flip | coord_polar
 
 ## 数据转换
@@ -42,3 +44,22 @@ ggplot(data = <data>) +
 ## 数据类型
 tibble格式相对于数据框格式有显示类型和不会强制转换因子等优点
 1. 数字:parse相对于as有许多优点，比如对于'$22%'字符串转换，as会报错，parse则会忽视数字前方的非数字，并且对于12，100这些其他计数方式，parse也能设置locale参数处理
+
+## 模型构建
+1. 模型公式中x^2也需要加I()转换符，要不然会直接当成x
+2. 模型正交多项式可以帮助简化多项式，模型构建一样，但是参数不同
+3. data_grid()和seq_range()能分别处理类别重复变量和连续变量，生成的变量能用于模型的预测，同时参数trim和expand对于拖尾数据等也有较好效果
+4. add-predictions、gather_predictions和spread_predictions都能根据模型增加预测值(残差)，但是后两者是针对多个模型，常见新的模型变量，方便绘图比较
+    - 多项式回归：poly(x,degree),注意数据外推
+    - 样条回归：需要用到spline包，ns()，geom_line()加线条时，需要指定aes(y = pred)
+5. model_matrix()能显示模型构建中的细节，如样条
+
+!!!note 循环变量命名中，不能用赋值符号，需要用到assign，如assign(paste0('mod',i),model)
+r中的lm函数会直接将多因子变量转换为哑变量进行分析
+
+- 回归的种类
+    1. glm:广义线性模型，普通的线性模型要求响应值是连续的，并且要求误差项呈正态分布。广义线性模型拓展了响应值的类型，并且对距离的度量是根据似然来估计
+    2. mgcv::gam：拓展glm，使其能包含任意的平滑函数，后续加上一定的约束平滑项
+    3. glmnet:glmnet：惩罚线性回归使用最大似然估计，在线性模型(logistic,cox等都能用）中加入惩罚项，如lasso，岭回归等
+    4. MASS::rlm:稳健回归，适合处理异常值
+    5. rpart:rpart():回归树，分段的常数模型，常常需要搭配先验知识(prior)
--- r4ds.r
+++ r4ds.r
@@ -42,4 +42,146 @@ flights %>%
   arrange(delay)
 
 diamonds <- diamonds
-diamonds %>% ggplot(mapping = aes(x = carat,color =cut)) + geom_freqpoly(binwidth = 0.1)
+diamonds %>% ggplot(mapping = aes(x = carat)) + 
+  geom_histogram(binwidth = 0.01) +
+  theme_bw()
+diamonds %>% 
+  filter(carat == 0.99 | carat == 1) %>% 
+  group_by(carat) %>% 
+  count()
+diamonds2 <- diamonds %>% 
+  filter(between(y, 3, 20))
+diamonds2 %>% 
+  count(cut,color) %>% 
+  ggplot(mapping = aes(x = cut,y = color))+
+  geom_tile(mapping = aes(fill = n))
+diamonds2 %>% 
+  ggplot(mapping = aes(x = price, y = carat)) + 
+  geom_bin2d(mapping = aes())
+df <- data.frame(abc = 1, xyz = "a")
+df$x
+df[, "xyz"]
+df[, c("abc", "xyz")]
+
+# response ----------------------------------------------------------------
+
+models <- tibble(
+  a1 = runif(250, -20, 40),
+  a2 = runif(250, -5, 5))
+data_sim <- sim1 %>% 
+  select(-pre)
+
+
+# predict -----------------------------------------------------------------
+
+model1 <- function(model,data){
+  model[1] + model[2] * data[['x']]
+}
+mesure_distance <- function(mod,data){
+  diff <- data[['y']] - model1(model = mod,data = data)
+  sqrt(mean(diff^2))
+}
+sim1_distance <- function(a1,a2,data){
+  mesure_distance(mod = c(a1,a2),data = sim1)
+}
+
+(models <- models %>% 
+  mutate(dist = map2_dbl(.x = a1,.y = a2,.f = sim1_distance)))
+ggplot(data = sim1,mapping = aes(x,y))+
+  geom_point()+
+  geom_abline(data = filter(.data = models,min_rank(dist)<10),
+              mapping = aes(intercept = a1,slope = a2,color = -dist))+
+  scale_x_continuous(limits = c(0,15))
+
+ggplot(data = models,mapping = aes(a1,a2))+
+  geom_point(data = filter(models,min_rank(dist)<=10),size = 4,color = 'red')+
+  geom_point(aes(color = -dist))
+
+(sim1 <- sim1 %>% 
+  add_residuals(model = lm(y~x,data = sim1)) %>% 
+  add_predictions(model = lm(y~x,data = sim1)) %>% 
+    select('x','y','pred',everything()))
+
+sim1 %>% 
+  ggplot(mapping = aes(x = x, y = resid)) + 
+  geom_point(data = filter(sim1,min_rank(abs(resid))<= 10),size = 2,shape = 2)+ 
+  geom_point(alpha = 0.5) +
+  coord_cartesian(xlim = c(1,10))+
+  geom_hline(yintercept = 0, color = 'red') + 
+  geom_refline(h = 0)
+
+
+# loess -------------------------------------------------------------------
+
+model_loe <- loess(formula = y~x,data = sim1)
+sim1 <- sim1 %>% 
+  add_predictions(model = model_loe) %>% 
+  add_residuals(model = model_loe)
+ggplot(data = sim1,mapping = aes(x = x,y = pred)) + 
+  geom_point() + 
+  geom_smooth() 
+
+geom_refline <- function(h,v,color = 'white'){
+  if(!missing(h)){
+    geom_hline(yintercept = h,size = 2, color = color)
+  }else if(!missing(v)){
+    geom_vline(xintercept = v, size = 2 ,color = color)
+  }else {
+    stop('stop')
+  }
+}
+
+
+# two predictor(cc) -------------------------------------------------------
+
+mod1 <- lm(y~x1 + x2,data = sim3)
+mod2 <- lm(y~x1 * x2, data = sim3)
+sim3 %>% 
+  gather_predictions(mod1,mod2) %>% 
+  ggplot(mapping = aes(x1,y,color = x2)) + 
+  geom_point() + 
+  geom_line(aes(y = pred)) + 
+  facet_wrap(~model)
+
+
+sim3 %>% 
+  gather_predictions(mod1,mod2) %>% 
+  gather_residuals(mod1,mod2) %>% 
+  ggplot(mapping = aes(x1,resid,color = x2)) + 
+  geom_point() + 
+  facet_grid(model~x2)
+
+sim5 <- tibble(
+  x = seq(0, 3.5 * pi, length = 50),
+  y = 4 * sin(x) + rnorm(length(x))
+)
+summary(lm(y~x^2+x,data = sim5))
+summary(lm(y~I(x^3)+I(x^2)+x,data = sim5))
+summary(lm(y~x,sim5))
+summary(lm(y~poly(x,3),sim5))
+sim5 %>% 
+  add_predictions(model = lm(y~poly(x,3),sim5))
+sim5 %>% 
+  add_predictions(model = lm(y~I(x^3)+I(x^2)+x,data = sim5))
+
+
+# splines 样条-----------------------------------------------------------------
+
+ggplot(sim5,mapping = aes(x,y)) + 
+  geom_point()
+for (i in 1:5) {
+ assign(paste0('mod',i), lm(formula = y~ns(x,i),data = sim5))
+}
+grid <- sim5 %>% 
+  data_grid(x = (seq_range(x = x,n = 50,expand = 0.1))) %>% 
+  gather_predictions(mod1,mod2,mod3,mod4,mod5) 
+ 
+ggplot(data = sim5,mapping = aes(x = x,y = y)) + 
+  geom_point() + 
+  geom_line(data = grid,mapping = aes(y = pred),color = 'red',alpha = 0.5) + 
+  facet_wrap(~model,nrow = 3)
+
+summary(lm(y~x1 * x2,sim3))
+model.matrix(lm(y~x1 + x2 ,sim3))
+sim3 %>% 
+  add_predictions(model = lm(y~x1 + x2,sim3))
